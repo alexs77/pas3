d1cada8a706c16773543a49852cd9ef96ea1a861
a.skwar <a.skwar@51bbfaec-a955-0410-bf18-b1cd311f72e3>
2008-09-06 18:49:32 +0000

    Add initial files.

---
+++ trunk/paras3/src/paras3.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python -OO
+# vim: set fileencoding=utf-8
+
+#===============================================================================
+# $LastChangedDate$
+# $LastChangedRevision$
+# $LastChangedBy$
+# $HeadURL$
+# $Id$
+#===============================================================================
+
+# This is paras3, the parallel s3 sync.
+# See <http://code.google.com/p/pas3/wiki/paras3> for more information.
+
+# The program is released under GPLv3.
+
+
